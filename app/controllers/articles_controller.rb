f9802a4f75386f2abaf04386904c3e824892b249
apeloza <apeloza@alumni.uwo.ca>
2016-08-12 16:09:07 -0500

    Added Update and Delete functionality + comments and auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -6,13 +8,37 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   def new
+    @article = Article.new
+  end
+  def edit
+    @article = Article.find(params[:id])
   end
   def create
     @article = Article.new(article_params)
 
-    @article.save
+    if @article.save
     redirect_to @article
+  else
+    render 'new'
   end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
